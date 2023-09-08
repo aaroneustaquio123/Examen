3c844ef5795e240e797518bc410c1d051a663116
Abel Aaron <142622615+aaroneustaquio123@users.noreply.github.com>
2023-09-08 13:42:40 -0500

    Add files via upload

---
+++ src/aaron/Aaron.java
@@ -0,0 +1,18 @@
+package aaron;
+
+
+public class Aaron {
+
+    
+    public static void main(String[] args) {
+       
+         ListaPersona Ventana = new ListaPersona();
+        Ventana.setLocationRelativeTo(Ventana);
+        Ventana.setVisible(true); 
+        
+        
+        
+        
+    }
+    
+}
---
+++ src/aaron/Persona.java
@@ -0,0 +1,28 @@
+package aaron;
+
+
+
+
+public class Persona {
+    private String Nombre;
+ 
+    public Persona()
+    {}
+    
+    public Persona(String Nombre)
+    {   this.Nombre = Nombre;
+
+    }
+
+      public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+   
+      public String getNombre() {
+        return Nombre;
+    }
+
+   
+    
+}
